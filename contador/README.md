706b75f0f9347622c72f576b1b5de0772c07e57e
yoruwitch <yoru.witch4@gmail.com>
2022-03-13 17:25:34 -0300

    First release of counter

--- contador/README.md
+++ contador/README.md
@@ -1,5 +1,9 @@
 # Contador
 
-## Assets
+### Description:
+* My first Flutter app. It's a simple counter, which has two buttons: one increases the number of people in the place and other decreases. 
+* There is a simple display, counting the numbers up to a limit.
 
-* Classroom blurred image found here in ![Freepik](a href='https://www.freepik.com/photos/school-room) photo created by tirachard.
+### Assets:
+
+* Classroom blurred image found here in [Freepik](a href='https://www.freepik.com/photos/school-room) photo created by tirachard.
--- contador/lib/main.dart
+++ contador/lib/main.dart
@@ -4,82 +4,121 @@ void main() {
   runApp(const MyApp());
 }
 
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+class HomePage extends StatefulWidget {
+  const HomePage({ Key? key }) : super(key: key);
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  
+  int count = 0;
+  String title = 'Can come in';
   void decrement() {
-    print('Decrement');
+    setState(() {
+      if(count > 0) {
+        title = 'Can come in';
+        count--;
+      }
+      
+    });
   }
 
   void increment() {
-    print('Increment');
+    setState(() {
+      if (count < 20) {
+        count++;
+      }
+      if (count == 20) {
+        title = 'The classroom is full';
+      }
+
+    });
+    
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.yellow,
-      body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-        const Text(
-          'Pode entrar',
-          style: TextStyle(
-              fontSize: 30, color: Colors.white, fontWeight: FontWeight.w700),
-        ),
-        const SizedBox(
-          height: 32,
-        ),
-        const Text('0', style: TextStyle(fontSize: 100, color: Colors.white)),
-        const SizedBox(
-          height: 32,
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextButton(
-                onPressed: decrement,
-                style: TextButton.styleFrom(
-                    backgroundColor: Colors.white,
-                    fixedSize: const Size(100, 100),
-                    primary: Colors.black,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(16))),
-                child: const Text(
-                  'saiu',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 16,
-                  ),
-                )),
-            const SizedBox(
-              width: 64,
+      body: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/classroom-blurred.jpg'),
+              fit: BoxFit.cover,
             ),
-            TextButton(
-                onPressed: increment,
-                style: TextButton.styleFrom(
-                    backgroundColor: Colors.white,
-                    fixedSize: const Size(100, 100),
-                    primary: Colors.black,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(16))),
-                // ignore: prefer_const_constructors
-                child: Text(
+          ),
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+             Text(
+              title,
+              style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w700),
+            ),
+            const SizedBox(
+              height: 32,
+            ),
+             Text('$count' ,
+              style: const TextStyle(fontSize: 100, color: Colors.white)),
+            const SizedBox(
+              height: 32,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                    onPressed: decrement,
+                    style: TextButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        fixedSize: const Size(100, 100),
+                        primary: Colors.black,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16))),
+                    child: const Text(
+                      'Out',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16,
+                      ),
+                    )),
+                const SizedBox(
+                  width: 64,
+                ),
+                TextButton(
+                    onPressed: increment,
+                    style: TextButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        fixedSize: const Size(100, 100),
+                        primary: Colors.black,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16))),
                     // ignore: prefer_const_constructors
-                    'entrou',
-                    style: const TextStyle(color: Colors.black, fontSize: 16)))
-          ],
-        ),
-      ]),
+                    child: Text(
+                        // ignore: prefer_const_constructors
+                        'In',
+                        style:
+                            const TextStyle(color: Colors.black, fontSize: 16)))
+              ],
+            ),
+          ])),
     );
   }
 }
+
+
+

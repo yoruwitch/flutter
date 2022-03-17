2bebeb10924d15ac92eaf346e583d6773c07a4ee
yoruwitch <yoru.witch4@gmail.com>
2022-03-17 20:41:12 -0300

    Ongoing project updated

--- todo-list/todo_list/lib/pages/todo_list_page.dart
+++ todo-list/todo_list/lib/pages/todo_list_page.dart
@@ -1,23 +1,77 @@
 import 'package:flutter/material.dart';
 
 class TodoListPage extends StatelessWidget {
-  const TodoListPage({ Key? key }) : super(key: key);
+  const TodoListPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 16),
-          child: TextField(
-            decoration: InputDecoration(
-              labelText: 'email',
-              hintText: 'example@example.com',
-              border: OutlineInputBorder(),
-            ),
-          ),
-        ),
-      ),
+          child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Row(
+                    children: [
+                      const Expanded(
+                        child: TextField(
+                            decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: 'Add a new task',
+                                hintText: 'Ex: Complete the math list.')),
+                      ),
+                      const SizedBox(
+                        width: 8,
+                      ),
+                      ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          primary: Color.fromRGBO(210, 69, 235, 1),
+                          padding: EdgeInsets.all(15),
+                        ),
+                        child: const Icon(
+                          Icons.add,
+                          size: 30,
+                        ),
+                      )
+                    ],
+                  ),
+
+                  const SizedBox(height: 16,),
+
+                  ListView(
+                    shrinkWrap: true,
+                    children: const [
+                      ListTile(
+                        title: Text('Task #1'),
+                      )
+                    ],
+                  ),
+
+                  const SizedBox(height: 16,),
+
+
+                  Row(
+                    children: [
+                      const Expanded(
+                        child: Text('You have 0 unfinished tasks.')
+                        ),
+
+                      const SizedBox(width:16),
+
+                      ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          primary: Color.fromRGBO(210, 69, 235, 1),
+                          padding: EdgeInsets.all(15),
+                        
+                      ),
+                      child: Text('Clean all'),
+                       ) ],
+                  )
+                ],
+              ))),
     );
   }
 }

c0b3f593fd3d7d9282eb3d7491952586844201b5
yoruwitch <yoru.witch4@gmail.com>
2022-04-06 20:01:32 -0300

    Forth Project started

---
+++ currency_converter/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+
+const request = "https://api.hgbrasil.com/finance?key=dd0ee016";
+
+void main() async{
+
+  
+  
+
+  runApp(MaterialApp(
+    home: Container(
+
+    )
+  ));
+}
+
+Future<Map> getData() async {
+  http.Response response = await http.get(request);
+  return json.decode(response.body);
+}
+
+
+
